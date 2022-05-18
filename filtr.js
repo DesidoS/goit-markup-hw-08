e94514a14088399221b82928d3ac74db9767c71c
Oleksii Kravchenko <djdesidos@gmail.com>
2022-05-18 16:46:41 +0300

    js

---
+++ filtr.js
@@ -0,0 +1,86 @@
+(() => {
+  const filtrWebBtn = document.querySelector("[data-web-button]");
+  const onlyWebOne = document.querySelector("[only-web-one]");
+  const onlyWebTwo = document.querySelector("[only-web-Two]");
+  const onlyWebThree = document.querySelector("[only-web-Three]");
+  const onlyWebFour = document.querySelector("[only-web-Four]");
+  const onlyWebFive = document.querySelector("[only-web-Five]");
+  const onlyWebSix = document.querySelector("[only-web-Six]");
+  const onlyWebSeven = document.querySelector("[only-web-Seven]");
+
+
+  filtrWebBtn.addEventListener("click", () => {
+  
+    onlyWebOne.classList.toggle("add");
+    onlyWebTwo.classList.toggle("add");
+    onlyWebThree.classList.toggle("add");
+    onlyWebFour.classList.toggle("add");
+    onlyWebFive.classList.toggle("add");
+    onlyWebSix.classList.toggle("add");
+    onlyWebSeven.classList.toggle("add");
+  });
+})();
+(() => {
+    const filtrAppBtn = document.querySelector("[data-app-button]");
+    const onlyAppOne = document.querySelector("[only-app-one]");
+    const onlyAppTwo = document.querySelector("[only-app-Two]");
+    const onlyAppThree = document.querySelector("[only-app-Three]");
+    const onlyAppFour = document.querySelector("[only-app-Four]");
+    const onlyAppFive = document.querySelector("[only-app-Five]");
+    const onlyAppSix = document.querySelector("[only-app-Six]");
+    
+    
+    filtrAppBtn.addEventListener("click", () => {
+        
+        onlyAppOne.classList.toggle("add");
+        onlyAppTwo.classList.toggle("add");
+        onlyAppThree.classList.toggle("add");
+        onlyAppFour.classList.toggle("add");
+        onlyAppFive.classList.toggle("add");
+        onlyAppSix.classList.toggle("add");
+    });
+})();
+(() => {
+  const filtrDesignBtn = document.querySelector("[data-Design-button]");
+  const onlyDesignOne = document.querySelector("[only-Design-one]");
+  const onlyDesignTwo = document.querySelector("[only-Design-Two]");
+  const onlyDesignThree = document.querySelector("[only-Design-Three]");
+  const onlyDesignFour = document.querySelector("[only-Design-Four]");
+  const onlyDesignFive = document.querySelector("[only-Design-Five]");
+  const onlyDesignSix = document.querySelector("[only-Design-Six]");
+  const onlyDesignSeven = document.querySelector("[only-Design-Seven]");
+
+
+  filtrDesignBtn.addEventListener("click", () => {
+  
+    onlyDesignOne.classList.toggle("add");
+    onlyDesignTwo.classList.toggle("add");
+    onlyDesignThree.classList.toggle("add");
+    onlyDesignFour.classList.toggle("add");
+    onlyDesignFive.classList.toggle("add");
+    onlyDesignSix.classList.toggle("add");
+    onlyDesignSeven.classList.toggle("add");
+  });
+})();
+(() => {
+  const filtrMarketingBtn = document.querySelector("[data-Marketing-button]");
+  const onlyMarketingOne = document.querySelector("[only-Marketing-one]");
+  const onlyMarketingTwo = document.querySelector("[only-Marketing-Two]");
+  const onlyMarketingThree = document.querySelector("[only-Marketing-Three]");
+  const onlyMarketingFour = document.querySelector("[only-Marketing-Four]");
+  const onlyMarketingFive = document.querySelector("[only-Marketing-Five]");
+  const onlyMarketingSix = document.querySelector("[only-Marketing-Six]");
+  const onlyMarketingSeven = document.querySelector("[only-Marketing-Seven]");
+
+
+  filtrMarketingBtn.addEventListener("click", () => {
+  
+    onlyMarketingOne.classList.toggle("add");
+    onlyMarketingTwo.classList.toggle("add");
+    onlyMarketingThree.classList.toggle("add");
+    onlyMarketingFour.classList.toggle("add");
+    onlyMarketingFive.classList.toggle("add");
+    onlyMarketingSix.classList.toggle("add");
+    onlyMarketingSeven.classList.toggle("add");
+  });
+})();
